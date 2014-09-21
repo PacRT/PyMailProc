bd112283ad328c08988b2256bcd7eab14d1f636b
Chiradip Mandal <chiradip@chiradip.com>
2014-09-20 17:11:43 -0700

    ok ok

--- updateredis.py
+++ updateredis.py
@@ -5,7 +5,12 @@ import register
 
 lua = '''
 local ownuid = redis.call('GET', "username:" ..KEYS[1].. ":uid")
-local issuid = redis.call('GET', "username:" ..KEYS[2].. ":uid") -- change it to "GET email:<real email as KEYS[2]:uid"
+local issuid = redis.call('GET', "email:" ..KEYS[2].. ":uid")
+if not issuid then
+    issuid = redis.call('INCR', "global:getNextUserId")
+    redis.call('SET', "email:" ..KEYS[2].. ":uid", issuid)
+    redis.call('SET', "uid:" ..issuid.. ":email", KEYS[2])
+end
 redis.call('ZADD', "owner:"  ..ownuid..":docs", ARGV[1], ARGV[2])
 redis.call('ZADD', "issuer:"  ..issuid..":docs", ARGV[1], ARGV[2])
 redis.call('HMSET', "doc:"..ARGV[2], "owner.uid", ownuid , "issuer.uid", issuid)
@@ -20,13 +25,17 @@ def update_redis(owner,issuer,url, dname):
   rusername  = owner.split("@")[0]
   iusername = issuer.split("@")[0]
   idomain = issuer.split("@")[1]
+  print("Before calling LUA that updates document entry for the user")
+  print("Owner's email: ", owner)
+  print("Issuer's email: ", issuer)
+  print("Owner's user name: ", rusername)
   r_up(keys=[rusername, issuer], args=[ts,url])
 
-  if idomain == 'paperlessclub.org':
-    iuid = register.get_id(iusername)
-
-  elif register.get_by_email(issuer) is None:
-    register.register_user(r_server, iusername, issuer, password, dname, "inactive")
+  # if idomain == 'paperlessclub.org':
+  #   iuid = register.get_id(iusername)
+  #
+  # elif register.get_by_email(issuer) is None:
+  #   register.register_user(r_server, iusername, issuer, password, dname, "inactive")
 
 
 
